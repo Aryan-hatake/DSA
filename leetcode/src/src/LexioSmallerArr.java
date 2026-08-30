07a82d4bd427b6db5bea43b7515cc8220fee25f3
Aryan-hatake <aryangadhave202@gmail.com>
2026-08-30 23:45:51 +0530

    feat: lexicoSmaller arr bruteforce v2

--- leetcode/src/src/LexioSmallerArr.java
+++ leetcode/src/src/LexioSmallerArr.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class LexioSmallerArr {
     public static void main(String[] args) {
@@ -9,21 +11,48 @@ public class LexioSmallerArr {
 
         int limit = 15;
 
+        ArrayList<Integer> group = new ArrayList<>();
+        ArrayList<Integer> groupIdx = new ArrayList<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (((arr[i] > arr[j] && i<j) || (arr[i] - arr[j] < 0 && i - j > 0  )) &&  arr[i] - arr[j] <= limit   ) {
-                    swap(arr, i, j);
-
+        for (int i =0; i<arr.length;i++){
+            for (int j=0;j<arr.length;j++){
+                if(arr[i]-arr[j] <=limit && arr[i] - arr[j] >= 0 && i!=j){
+                    if(!group.contains(arr[i])){
+                        group.add(arr[i]);
+                        groupIdx.add(i);
+                    }
+                    if(!group.contains((arr[j]))){
+                        group.add(arr[j]);
+                        groupIdx.add(j);
+                    }
                 }
             }
         }
+
+        ArrayList<Integer> sortedGroup = new ArrayList<>(group);
+        ArrayList<Integer> sortedGroupIdx = new ArrayList<>(groupIdx);
+        Collections.sort(sortedGroup);
+        Collections.sort(sortedGroupIdx);
+
+        System.out.println(group);
+
+      for(int i =0; i<sortedGroup.size();i++){
+          replace(arr,sortedGroupIdx.get(i), sortedGroup.get(i));
+      }
+
         System.out.println(Arrays.toString(arr));
     }
 
-    static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+
+    static void replace(int[] arr, int idx, int num) {
+        arr[idx] = num;
+    }
+    static int originalIdx(int[] arr, int ele) {
+        for (int i =0; i< arr.length;i++){
+            if(arr[i] == ele){
+                return i;
+            }
+        }
+        return -1;
     }
 }
