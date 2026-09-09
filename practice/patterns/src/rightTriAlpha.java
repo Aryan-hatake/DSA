6f8ff6c6778d45900744af5cee44d4968d886930
Aryan-hatake <aryangadhave202@gmail.com>
2026-09-09 22:37:32 +0530

    feat: inverted  right triangle   patterns

--- practice/patterns/src/rightTriAlpha.java
+++ practice/patterns/src/rightTriAlpha.java
@@ -1,5 +1,6 @@
 public class rightTriAlpha {
     public static void main(String[] args) {
+        int n= 5;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print((char)('A' + j - 1 ) + " ");
@@ -8,4 +9,4 @@ public class rightTriAlpha {
         }
     }
     }
-}
+
